2b2b4319b85ca6239403c01e134c56fee2851366
Maddo <mjablecki@gmail.com>
2013-04-12 02:32:28 -0300

    basic modules for installing packages

---
+++ manifests/base.pp
@@ -0,0 +1,22 @@
+stage { init: before => Stage['main'], }
+
+class dependencies {
+    notify {"whats up homie":}
+    class { apt::update: }
+
+    # this installs php5 with apache :(
+    # class { 'php':
+    #     require => Class[apt::update:]
+    # }
+}
+
+class {'dependencies': stage => init}
+
+$packages = [
+    "php5-cgi",
+    "php5-cli",
+    "nginx",
+    "mysql"
+]
+
+package { $packages: ensure => latest, }
